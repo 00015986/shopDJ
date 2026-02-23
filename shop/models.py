b2dce79636101106096cc09fb0027a75f123b8e2
00015986 <00015986wiut@gmail.com>
2026-02-23 11:55:40 -0600

    Add Category, Product, and Tag models with relationships

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Category(models.Model):          # Model 1
+class Category(models.Model):          # Category model for Many-to-One relationship with Product
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
@@ -12,7 +12,7 @@ class Category(models.Model):          # Model 1
         return self.name
 
 
-class Tag(models.Model):               # Model 2 (for many-to-many)
+class Tag(models.Model):               # TAG Model 2 many to many with Product
     name = models.CharField(max_length=50, unique=True)
 
 
@@ -20,11 +20,11 @@ class Tag(models.Model):               # Model 2 (for many-to-many)
         return self.name
 
 
-class Product(models.Model):           # Model 3
+class Product(models.Model):           # Product model with Many-to-One relationship to Category and Many-to-Many relationship to Tag
     category    = models.ForeignKey(Category, on_delete=models.CASCADE,
                                     related_name='products')   # MANY-TO-ONE
     tags        = models.ManyToManyField(Tag, blank=True)       # MANY-TO-MANY
-    name        = models.CharField(max_length=200)
+    name        = models.CharField(max_length=200)  
     slug        = models.SlugField(unique=True)
     description = models.TextField(blank=True)
     price       = models.DecimalField(max_digits=10, decimal_places=2)
