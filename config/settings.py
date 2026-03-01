b3f5d210554e2dfb1f4327ed696b93c71b3de077
00015986 <00015986wiut@gmail.com>
2026-03-01 12:18:41 -0600

    Adjusting the Code for the Flask8 requirements

--- config/settings.py
+++ config/settings.py
@@ -15,9 +15,7 @@ from pathlib import Path
 from decouple import config
 
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
